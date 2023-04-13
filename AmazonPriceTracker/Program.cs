00a0d8e78445f8504a045a701c2dafed63caf7a3
Alan Spindler <alan.rafael@it4solution.com.br>
2023-04-13 18:08:49 -0400

    Commit inicial

---
+++ AmazonPriceTracker/Program.cs
@@ -0,0 +1,140 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text.Json;
+using Microsoft.Playwright;
+using File = System.IO.File;
+
+class AmazonPriceTracker
+{
+    private static async Task<decimal?> GetProductPriceAsync(IPage page, string url)
+    {
+        await page.GotoAsync(url);
+
+        // Substitua ".price-selector" pelo seletor CSS correto do preço do produto na Amazon
+        var priceElement = await page.QuerySelectorAsync("[class=a-price-whole] >> nth=1");
+        var priceText = await priceElement.InnerTextAsync();
+
+        // Analise o texto do preço para obter o valor decimal
+        if (decimal.TryParse(priceText, out decimal price))
+        {
+            return price;
+        }
+        return null;
+    }
+
+    private static (string Email, string Password) ReadEmailCredentials()
+    {
+        var json = File.ReadAllText("email_credentials.json");
+        var credentials = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+
+        return (credentials["email"], credentials["password"]);
+    }
+
+    private static List<string> ReadEmailRecipients()
+    {
+        return File.ReadAllLines("email_recipients.txt").ToList();
+    }
+
+    public static async Task SendEmail(string subject, string body)
+    {
+        var (email, password) = ReadEmailCredentials();
+        var recipients = ReadEmailRecipients();
+        try
+        {
+            using MailMessage mailMessage = new()
+            {
+                From = new MailAddress(email),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+
+            foreach (var recipient in recipients)
+            {
+                mailMessage.To.Add(new MailAddress(recipient));
+            }
+
+            using SmtpClient smtpClient = new("smtp.office365.com", 587)
+            {
+                Credentials = new NetworkCredential(email, password),
+                EnableSsl = true
+            };
+
+            try
+            {
+                await smtpClient.SendMailAsync(mailMessage);
+                Log($"E-mail enviado para: {mailMessage.To[0].Address}");
+            }
+            catch (SmtpException ex)
+            {
+                Log($"Erro ao enviar e-mail:\nCódigo de status: {ex.StatusCode}\nMensagem de erro: {ex.Message}\nMensagem de erro interna: {ex.InnerException?.Message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao enviar e-mail:");
+            Console.WriteLine($"Mensagem de erro: {ex.Message}");
+            if (ex.InnerException != null)
+            {
+                Console.WriteLine($"Mensagem de erro interna: {ex.InnerException.Message}");
+            }
+        }
+    }
+
+    private static void Log(string message)
+    {
+        File.AppendAllText("log.txt", $"{DateTime.Now}: {message}\n");
+    }
+
+    private static List<(string, decimal)> ReadProducts()
+    {
+        return File.ReadAllLines("products.txt")
+            .Select(line =>
+            {
+                var parts = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
+                return (parts[0], decimal.Parse(parts[1]));
+            })
+            .ToList();
+    }
+
+    public static async Task Main()
+    {
+        var productList = ReadProducts();
+
+        using var playwright = await Playwright.CreateAsync();
+        var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false });
+        var context = await browser.NewContextAsync();
+        var page = await context.NewPageAsync();
+
+        foreach (var (productUrl, targetPrice) in productList)
+        {
+            try
+            {
+                // Verificar o preço do produto
+                var currentPrice = await GetProductPriceAsync(page, productUrl);
+
+                if (currentPrice.HasValue && currentPrice.Value <= targetPrice)
+                {
+                    var subject = $"Alerta de preço: Produto abaixo de R${targetPrice}";
+                    var body = $"O produto na URL {productUrl} está com um preço de R${currentPrice.Value}.";
+                    await SendEmail(subject, body);
+                    Log("E-mail enviado.");
+                }
+                else
+                {
+                    Log($"O preço do produto não atingiu o valor desejado. Preço atual: R${currentPrice.Value}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log($"Erro: {ex.Message}");
+                var timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+                var screenshotPath = $"ErroAmazon{timestamp}.jpg";
+                await System.IO.File.WriteAllBytesAsync(screenshotPath, await page.ScreenshotAsync(new PageScreenshotOptions { Type = ScreenshotType.Jpeg }));
+                Log($"Screenshot do erro salvo em {screenshotPath}");
+            }
+        }
+        await browser.CloseAsync();
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# AmazonPriceTracker
-Tracking de preços da Amazon
+Amazon Price Tracker
+
+Este projeto é um rastreador de preços de produtos da Amazon, desenvolvido em C# e utilizando o Playwright. Ele verifica se o preço atual de uma lista de produtos está abaixo de um determinado valor e, se estiver, envia um e-mail de alerta para uma lista de destinatários.
+Requisitos
+
+    .NET 6.0 ou superior
+    Playwright para .NET
+
+Instalação
+
+    Clone o repositório ou baixe o projeto para sua máquina local.
+    No diretório do projeto, execute dotnet restore para instalar as dependências.
+    Crie um arquivo chamado email_credentials.json no diretório raiz do projeto com as credenciais do e-mail que será utilizado para enviar as notificações (exemplo de conteúdo abaixo):
+
+json
+
+{
+  "Email": "your_email@example.com",
+  "Password": "your_password"
+}
+
+    Crie um arquivo chamado email_recipients.txt no diretório raiz do projeto contendo uma lista de e-mails que receberão as notificações. Coloque um e-mail por linha, como no exemplo abaixo:
+
+recipient1@example.com
+recipient2@example.com
+
+    Crie um arquivo chamado products.txt no diretório raiz do projeto contendo uma lista de URLs dos produtos que você deseja rastrear e os respectivos preços-alvo. Coloque um produto por linha, separando a URL e o preço-alvo por vírgula, como no exemplo abaixo:
+
+bash
+
+https://www.amazon.com.br/Produto1/dp/XXXXX, 100
+https://www.amazon.com.br/Produto2/dp/XXXXX, 150
+
+    Compile e execute o projeto com dotnet build e dotnet run, respectivamente.
+	
+Instalando o navegador do Playwright
+
+    Faça o build da solução.
+    Abra o Powershell 6.0 ou superior.
+    Navegue até a pasta de compilação do projeto. Substitua <PATH_TO_BUILD_FOLDER> pelo caminho correto da pasta de compilação no seu computador.
+
+bash
+
+cd "<PATH_TO_BUILD_FOLDER>"
+
+Exemplo:
+
+cd "C:\AmazonPriceTracker\AmazonPriceTracker\bin\Debug\net6.0"
+
+    Execute o comando a seguir para instalar o navegador do Playwright:
+
+pwsh playwright.ps1 install
+
+Após seguir estas etapas, o navegador do Playwright estará instalado e pronto para ser usado no projeto.
+
+Pelo projeto compilado:
+
+- Necessita do PowerShell 6.0
+
+Descompacte o arquivo Projecto Compilado.zip
+
+Edite o arquivo email_credentials.json com suas credenciais de e-mail do hotmail (usuário e senha)
+Edite o arquivo email_recipients.txt com a lista de e-mails para os quais deseja enviar.
+Edite o arquivo products.txt com os produtos que deseja verificar e o preço de alerta.
+
+Execute o arquivo Instalar Playwright.bat
+Aguarde a instalação finalizar.
+Execute o arquivo AmazonPriceTracker.exe
+
+
+Funcionalidades
+
+    Rastrear preços de produtos da Amazon.
+    Enviar e-mails de alerta quando o preço do produto estiver abaixo do valor desejado.
+    Ler a lista de produtos e preços-alvo de um arquivo externo.
+    Ler a lista de destinatários de e-mail de um arquivo externo.
+    Utilizar credenciais de e-mail de um arquivo externo para maior segurança.
+
+Limitações
+
+    A implementação atual é específica para a Amazon Brasil.
+    O rastreamento de preços é feito de forma manual, ou seja, é necessário executar o programa toda vez que desejar verificar os preços dos produtos.
+
+Contribuindo
+
+Sinta-se à vontade para contribuir com melhorias ou correções de bugs. Faça um fork do projeto, crie uma branch, realize as alterações e envie um pull request.
